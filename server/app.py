489bb3c6cbd7a1c488a8c4336f9a1854bbc8fa86
Arnold-In-Tech <g16a7782@gmail.com>
2024-04-13 14:56:45 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -48,6 +48,59 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+
+class Login(Resource):
+    def post(self):
+        name = request.get_json()['username']
+        user_exists = User.query.filter(User.username == name).first()
+        
+        if user_exists:
+            session['user_id'] = user_exists.id
+            response = make_response(
+                jsonify(user_exists.to_dict()),
+                200
+            )
+            return response
+        else:
+            response = make_response(
+                jsonify({'message': "User does not exist, Please sign up"}),
+                400
+            )
+            return response
+
+api.add_resource(Login, '/login')
+
+
+class Logout(Resource):
+    def delete(self): 
+        session['user_id'] = None
+        response = make_response(
+            jsonify({'message': '204: No Content'}),
+            204,
+        )
+        return response
+api.add_resource(Logout, '/logout')
+
+        
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            response = make_response(
+                jsonify(user.to_dict()),
+                200,
+            )
+            return response
+        else:
+            response = make_response(
+                jsonify({}),            # returns no data and a 401 (Unauthorized) status code.
+                401,
+            )
+            return response
+
+api.add_resource(CheckSession, '/check_session')
+
+
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
